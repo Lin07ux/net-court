a22652f64154f190bbd4c41f8714dae62d8011f8
Lin07ux <linshengli.linux7@icloud.com>
2018-12-12 22:44:50 +0800

    init project

---
+++ src/NotaryClient.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Notary client
+ *
+ * Author: Lin07ux
+ * Created_at: 2018-12-12 22:48:51
+ */
+
+namespace NetCourt;
+
+
+class NotaryClient
+{
+    public function __construct (array $config)
+    {
+        //
+    }
+}
