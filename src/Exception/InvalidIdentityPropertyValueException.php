628274975cced7985152869180b2c17c176dc630
Lin07ux <linshengli.linux7@icloud.com>
2018-12-14 10:38:27 +0800

    feat: 增加非法 Identity 属性值异常

---
+++ src/Exception/InvalidIdentityPropertyValueException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Invalid property value of Identity
+ *
+ * Author: Lin07ux
+ * Created_at: 2018-12-14 09:45:32
+ */
+
+namespace NetCourt\Exception;
+
+class InvalidIdentityPropertyValueException extends \InvalidArgumentException {}
