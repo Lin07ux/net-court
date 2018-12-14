852bcbfbd8de0f7852414af1e61c47ea4766a573
Lin07ux <linshengli.linux7@icloud.com>
2018-12-14 16:10:41 +0800

    feat: 增加错误响应异常类

---
+++ src/Exception/BadResponseException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Bad Response Exception
+ *
+ * Author: Lin07ux
+ * Created_at: 2018-12-14 14:47:10
+ */
+
+namespace NetCourt\Exception;
+
+class BadResponseException extends \RuntimeException {}
